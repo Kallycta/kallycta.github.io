553e9b37a01f19fb65498aedb59ad28f1bbe0a54
Rustam Latipov <83584576+Kallycta@users.noreply.github.com>
2023-12-20 14:12:19 +0300

    Update createAccountForm.js

--- createAccountForm.js
+++ createAccountForm.js
@@ -260,15 +260,13 @@ const showError = (input, msg) => {
                         } else {
                             // console.log('no errorTextMes')
                         }
-                    } else {
+                    }  
                         // console.log('no XHR.response')
-                    }
                 }
-            }
         };
 
         XHR.open("POST", "https://api.leads.convolo.ai/api/v2/auth/register-self-onboarding"); 
         XHR.setRequestHeader("Content-type", "application/json");
         XHR.setRequestHeader("Access-Control-Allow-Origin", "*");
         XHR.send(sendObject);
-    }
+    } 
