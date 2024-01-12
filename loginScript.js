26836ac9c243f856a9fc986f7a4f467ea5b270fc
Rustam Latipov <83584576+Kallycta@users.noreply.github.com>
2024-01-12 10:57:33 +0300

    Update loginScript.js

--- loginScript.js
+++ loginScript.js
@@ -1,1 +1,60 @@
+const email = document.querySelector("#email-2");
+const password = document.querySelector("#password");
+cosnt btn = document.querySelector("#btn-submit");
+
+
+
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim())) {
+        showSuccess(input)
+        return true
+    } else {
+        showError(input, 'Email is not invalid');
+        return false
+    }
+}
+
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    if (formControl) {
+        input.classList.remove('error');
+        formControl.classList.remove('error');
+        const small = formControl.parentElement.querySelector(".extra");
+        if (small) {
+            small.style.display = "none";
+        } 
+    }
+}
+const showError = (input) => {
+    const formControl = input.parentElement;
+    formControl.classList.add("error");
+    input.classList.add("error");
+    if (formControl) {
+        formControl.classList.add("error");
+
+        const small = formControl.parentElement.querySelector(".extra");
+        if (small) {
+            small.style.display = "block";
+            // small.textContent = msg;
+        } else {
+            const smallParent = formControl.parentElement;
+            const smallPhone = smallParent.parentElement.querySelector(".extra");
+            if (smallPhone) {
+                smallPhone.style.display = "block";
+                // smallPhone.textContent = 'Phone not valid';
+            }
+        }
+
+    }
+};
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    checkEmail(email);
+})
+
+
+
+// https://api.leads.convolo.ai/api/v1/auth/login
 
