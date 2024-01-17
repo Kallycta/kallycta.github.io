380e268e01c948fc68d1ef1e9372514d49fad79c
Rustam Latipov <83584576+Kallycta@users.noreply.github.com>
2024-01-17 14:17:18 +0300

    Update loginScript.js

--- loginScript.js
+++ loginScript.js
@@ -104,7 +104,7 @@ const validateForm = () => {
                         // window.location.href = `http://localhost:4201/?is_login=${myobj.token}&current_page=/pages/dashboard`
                       // window.location.href = `http://localhost:4201/pages/dashboard?is_login=${myobj.token}&current_page=/pages/dashboard`
                         setTimeout(()=> {
-                            window.open('/self-onboarding', '_self');
+                            window.open('http://localhost:4201/pages/dashboard?is_login=${myobj.token}&current_page=/pages/dashboard', '_self');
                         }, 1500)
                        
                     } else {
